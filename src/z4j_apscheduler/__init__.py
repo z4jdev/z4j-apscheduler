1f5348c76372cc04c1d7f9c76d2727e14b711cce
pypv <106410335+pypv@users.noreply.github.com>
2026-05-03 00:51:14 -0400

    Release 1.4.0

--- src/z4j_apscheduler/__init__.py
+++ src/z4j_apscheduler/__init__.py
@@ -11,6 +11,6 @@ from __future__ import annotations
 
 from z4j_apscheduler.scheduler import APSchedulerAdapter
 
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 
 __all__ = ["APSchedulerAdapter", "__version__"]
